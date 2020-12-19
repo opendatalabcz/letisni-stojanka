3bdb82d75b264bbebccfd90348e522c7c42b2642
Oliver <blaskoli@fit.cvut.cz>
2020-12-19 21:19:57 +0100

    Re-factored data preprocesing file

---
+++ constants.py
@@ -0,0 +1,18 @@
+CLASS_NAMES = {"0": "airplane",\
+                "1": "cargo-door", \
+               "2": "jet-bridge", \
+               "3": "tank-truck", \
+               "4": "cargo-truck", \
+               "5": "push-back-truck", \
+               "6": "luggage-loading-truck", \
+               "7": "cargo-box", \
+               "8": "basic-truck", \
+               "9": "passenger-bus", \
+               "10": "person",}
+
+VIDEO_PATH = "/home/oliver/School/THESIS/data/hong_kong/hong_kong.mp4"
+JSON_PATH = "/home/oliver/School/THESIS/data/hong_kong/instances_default.json"
+EXAMPLE_IMG = "/home/oliver/School/THESIS/data/hong_kong/frames/frame_000125.jpg"
+ANNOTATION_PATH = "/home/oliver/School/THESIS/data/hong_kong/annotations"
+
+DATA_PATH = "/home/oliver/School/THESIS/data/hong_kong"
--- preprocess_data.py
+++ preprocess_data.py
@@ -2,25 +2,24 @@ import tensorflow as tf
 import json
 import numpy as np
 import IPython.display as display
-#import cv2 
+import cv2 
 import os 
 import random
 import math
 import time 
-VIDEO_PATH = './hong_kong.mp4'
+
+import constants
+
 FRAMES_PATH = './data'
 FRAMES_CUT = 17500
 
 DATA_PATH = "/home/oliver/School/SUMMER_2020/airport-apron-object-detection/data/hong_kong"
 
-class_names = {"0": "airplane", "1": "pushback_car", "2": "cargo_door", "3": "aeroplane", "4": "jet_bridge", "5": "tank_truck", "6": "cargo_truck", \
-               "7": "luggage_loading_truck", "8": "cargo_box", "9": "truck", "10": "passenger_bus"}
-
 def extract_frames(video_path, frames_path):
     cam = cv2.VideoCapture(video_path)   
-    os.makedirs('data') 
+    os.makedirs("frames") 
     # frame 
-    currentframe = 0
+    current_frame = 0
     
     while(True): 
         # reading from frame 
@@ -28,7 +27,7 @@ def extract_frames(video_path, frames_path):
     
         if ret: 
             # if video is still left continue creating images 
-            name = frames_path + '/frame_' + str(currentframe).zfill(6) + '.jpg'
+            name = frames_path + '/frame_' + str(current_frame).zfill(6) + '.jpg'
             #print ('Creating...' + name) 
     
             # writing the extracted images 
@@ -36,7 +35,7 @@ def extract_frames(video_path, frames_path):
     
             # increasing counter so that it will 
             # show how many frames are created 
-            currentframe += 1
+            current_frame += 1
         else: 
             break
     
@@ -47,10 +46,8 @@ def extract_frames(video_path, frames_path):
 def get_img_size(img_path):
     # img_path = './sample_video/data/frame_000125.jpg'
     im = cv2.imread(img_path)
-    img_height, img_weight, c = im.shape
-    print('width:  ', img_weight)
-    print('height: ', img_height)
-    print('channel:', c)
+    h, w, c = im.shape
+    return h, w
 
 RAW_ANNOTATION_PATH = "/home/oliver/School/SUMMER_2020/airport-apron-object-detection/data/hong_kong/raw_annotations"
 ANNOTATIONS_PATH = "/home/oliver/School/SUMMER_2020/airport-apron-object-detection/data/hong_kong/annotations"
@@ -58,7 +55,7 @@ IMG_PATH = "/home/oliver/School/SUMMER_2020/airport-apron-object-detection/data/
 
 def extract_annotations(json_path, annotation_path):
     # opening JSON file 
-    json_file = open(json_path + "/instances_default.json")  
+    json_file = open(json_path)  
     
     # returns JSON object as a dictionary 
     anotation_data = json.load(json_file) 
@@ -67,19 +64,18 @@ def extract_annotations(json_path, annotation_path):
 
     # mapping image_id to corresponding filename
     for d in anotation_data['images']: 
-        img_ids[d["id"]] = d["file_name"]
+        img_ids[d["id"]] = d["file_name"][:-3] + "jpg"
         #print(d["id"], d["file_name"]) 
 
-    img_weight = 1278
-    img_height = 720
+    img_h, img_w = get_img_size(constants.EXAMPLE_IMG)
     #iterate over all bounding boxes
     for d in anotation_data['annotations']:
-        x = d["bbox"][0]/img_weight
-        y = d["bbox"][1]/img_height
-        w = d["bbox"][2]/img_weight
-        h = d["bbox"][3]/img_height
+        x = d["bbox"][0]/img_w
+        y = d["bbox"][1]/img_h
+        w = d["bbox"][2]/img_w
+        h = d["bbox"][3]/img_h
         
-        img_annotation_fname = annotation_path + "/" + img_ids[d["image_id"]] + ".txt"
+        img_annotation_fname = annotation_path + "/" + img_ids[d["image_id"]][:-3] + "txt"
         
         if os.path.exists(img_annotation_fname):
             append_write = 'a' # append if already exists
@@ -118,15 +114,21 @@ def train_test_split(img_path, frame_rate):
     print(fnames)
 
 
-def test_annotaion(annotation_path):
+def test_annotaion(data_path):
+    
+    #create testing folder
+    os.popen("mkdir ./test_dir")
+    annotation_path = data_path + "/annotations"
+    frames_path = data_path + "/frames"
+
     fnames = []
     for fname in os.listdir(annotation_path):
         fnames.append(fname)
     
-    for i in range(3):
+    for i in range(10):
 
         fname = random.choice(fnames)
-        old_fname = IMG_PATH + "/" + fname[:-3] + "jpg" 
+        old_fname = frames_path + "/" + fname[:-3] + "jpg" 
         os.popen('cp {} ./test_dir/'.format(old_fname))
         time.sleep(1)
 
@@ -134,7 +136,7 @@ def test_annotaion(annotation_path):
         f = open(ann_fname, "r")
         img = cv2.imread('./test_dir/' + fname[:-3] + "jpg")
         
-        img_height, img_weight, c = img.shape
+        img_h, img_w, c = img.shape
         
         for line in f:
             splits = line.split()
@@ -142,19 +144,21 @@ def test_annotaion(annotation_path):
             bbox = [splits[1], splits[2], splits[3], splits[4]]
             print("Class id - {}, bbox[{},{},{},{}]".format(class_id, bbox[0], bbox[1], bbox[2], bbox[3]))
 
-            x = int(float(bbox[0]) * img_weight)
-            y = int(float(bbox[1]) * img_height)
+            x = int(float(bbox[0]) * img_w)
+            y = int(float(bbox[1]) * img_h)
 
-            x2 = x + int(float(bbox[2]) * img_weight)
-            y2 = y + int(float(bbox[3]) * img_height)
+            x2 = x + int(float(bbox[2]) * img_w)
+            y2 = y + int(float(bbox[3]) * img_h)
 
             img = cv2.rectangle(img, (x,y), (x2,y2), (0,0,255), 2)
-            img = cv2.putText(img, class_names[str(int(class_id))], (x+1,y+1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, 255)
+            img = cv2.putText(img, constants.CLASS_NAMES[str(int(class_id))], (x+1,y+1), cv2.FONT_HERSHEY_SIMPLEX, 0.7, 255)
         
         #cv2.imwrite(old_fname, img) 
         cv2.imshow('image',img)
         cv2.waitKey(0)
 
+    #clean testing files
+    os.popen("rm -rf ./test_dir")
         
 def get_filenames(path):
     file = open(path, "r")
@@ -174,9 +178,10 @@ def zip_data():
     command = os.popen('zip {}/my_data.zip {}'.format(DATA_PATH, all_fnames))
 
 
-#extract_annotations(JSON_PATH, JSON_PATH)
-train_test_split("/home/blaskoli/data", 10)
-#test_annotaion(ANNOTATIONS_PATH)
+#extract_annotations(constants.JSON_PATH, constants.ANNOTATION_PATH)
+test_annotaion(constants.DATA_PATH)
+
+#train_test_split("/home/blaskoli/data", 10)
 #test_annotaion("/home/oliver/School/SUMMER_2020/airport-apron-object-detection/data/hong_kong/raw_annotations")
 #zip_data()    
 
