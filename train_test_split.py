ef2db0b02e61b15696a7600fc26301b868e28b37
Oliver <blaskoli@fit.cvut.cz>
2020-12-20 02:30:35 +0100

    Typo in video path

--- train_test_split.py
+++ train_test_split.py
@@ -3,7 +3,8 @@ import random
 import math
 
 FRAMES_PATH = "/home/oliver/School/THESIS/data/hong_kong/frames"
-TOTAL_FRAMES = 50421
+OUT_PATH = "/home/blaskoli/frames"
+TOTAL_FRAMES = 50422
 FRAME_RATE = 30
 TRAIN_SPLIT = 0.75
 VALID_SPLIT = 0.15
@@ -12,7 +13,7 @@ def train_test_split(frames_path, frame_rate):
     #get all frames fnames and sort them
     fnames = []
     for fname in sorted(os.listdir(frames_path)):
-        fnames.append(frames_path + "/" + fname)
+        fnames.append(OUT_PATH + "/" + fname)
 
     #filter every n-th frame given by frame_rate parameter
     filtered_fnames = []
@@ -28,9 +29,9 @@ def train_test_split(frames_path, frame_rate):
     train_threshold = math.floor(total_filtered_frames*TRAIN_SPLIT)
     valid_threshold = math.floor(total_filtered_frames*VALID_SPLIT) + train_threshold
 
-    train_file = open("./data_split/train.txt", "w")
-    valid_file = open("./data_split/valid.txt", "w")
-    test_file = open("./data_split/test.txt", "w")
+    train_file = open("./model/train.txt", "w")
+    valid_file = open("./model/valid.txt", "w")
+    test_file = open("./model/test.txt", "w")
     
     for i in range(0, train_threshold):
         train_file.write(filtered_fnames[i] + "\n")
